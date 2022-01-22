c0a8a09acc67f2210952d918cd750b575e088fcf
5010TigerDynasty <roboticsteam.fhs@gmail.com>
2022-01-22 12:49:21 -0500

    updated gradle version
    no more losing control of the robot

--- RapidReact2022/src/main/java/frc/robot/commands/ComboDrive.java
+++ RapidReact2022/src/main/java/frc/robot/commands/ComboDrive.java
@@ -38,7 +38,7 @@ public class ComboDrive extends CommandBase {
     //driveSubsystem.curvatureDrive();
 
     //switches from arcade to curvature at a bound
-    if(Math.abs(throttle) < .80){
+    if(Math.abs(throttle) < .90){
       driveTrain.arcadeDrive(throttle, steer);
       SmartDashboard.putString("Mode", "arcade");
     }else{
